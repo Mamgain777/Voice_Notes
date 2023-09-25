fa7cf21334c1173464f139bd152dcb4da9cddcd2
Himanshu Mamgain <mamgainjii@gmail.com>
2023-09-25 20:24:13 +0530

    simple jwt login authentication bug fix

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from api import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    # TokenVerifyView,
+)
 
 app_name = 'api'
 
@@ -8,4 +13,7 @@ urlpatterns = [
     path("notes", views.notes_api, name='notes'),
     path("notes/<int:pk>", views.note_api, name='note'),
     path("create/", views.create, name='create'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
