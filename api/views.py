ef4053278fe0f3996bdef7f59f460a2e20ac4682
Himanshu Mamgain <mamgainjii@gmail.com>
2023-09-25 19:36:46 +0530

    User password not storing bug fix

--- api/views.py
+++ api/views.py
@@ -81,7 +81,8 @@ def create(request):
         result = User.objects.filter(username = username)
         if(len(result) == 0):
 
-            new_user = User(username = username,password=password)
+            new_user = User(username = username)
+            new_user.set_password(password)
             new_user.save()
             return Response('sucess')
         else:
